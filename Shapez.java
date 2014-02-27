53d363bd371f5bea3e1482a17e3ff790660d2a02
Jacob Mischka <x.mischka.x@gmail.com>
2014-02-27 11:46:28 -0600

    Finished the Java version

--- Shapez.java
+++ Shapez.java
@@ -3,7 +3,69 @@ import java.lang.Math;
 
 public class Shapez{
 	public static void main(String[] args){ //should use Scanner, File I/O, while/for loops "
+		String moreInput = "y";
+		Scanner s = new Scanner(System.in);
+		String shape = "";
+		Point point;
+		Circle circle;
+		Rectangle rect;
 		
+		while(moreInput != null && moreInput.equalsIgnoreCase("y")){
+			System.out.print("Enter the x coordinate for the point: ");
+			double pointX = s.nextDouble(); s.nextLine();
+			System.out.print("Enter the y coordinate for the point: ");
+			double pointY = s.nextDouble(); s.nextLine();
+			point = new Point(pointX, pointY);
+			System.out.print("Enter a C to create a circle or an R to create a rectangle: ");
+			shape = s.nextLine();
+			if(shape.equalsIgnoreCase("c")){
+				System.out.print("Enter the coordinates for the circle's centerpoint, separated by a space (x y): ");
+				double cX = s.nextDouble();
+				double cY = s.nextDouble();
+				s.nextLine();
+				System.out.print("Enter the circle's radius: ");
+				double cR = s.nextDouble(); s.nextLine();
+				circle = new Circle(new Point(cX, cY), cR);
+				
+				System.out.println("The area of the circle is " + circle.area());
+				System.out.println("The circumference of the circle is " + circle.circumference());
+				
+				if(circle.contains(point))
+					System.out.println("The point is within the circle");
+				else
+					System.out.println("The point is not in the circlce");
+			}
+			else if(shape.equalsIgnoreCase("r")){
+				System.out.println("Enter the coordinates for the rectangle's upper-left corner, separated by a space (x y): ");
+				double ulX = s.nextDouble();
+				double ulY = s.nextDouble();
+				s.nextLine();
+				System.out.print("Enter the width of the rectangle: ");
+				double w = s.nextDouble(); s.nextLine();
+				System.out.print("Enter the height of the rectangle: ");
+				double h = s.nextDouble(); s.nextLine();
+				rect = new Rectangle(new Point(ulX, ulY), w, h);
+				
+				System.out.println("The area of the rectangle is " + rect.area());
+				System.out.println("The perimeter of the rectangle is " + rect.perimeter());
+				
+				if(rect.isSquare())
+					System.out.println("The rectangle is a square.");
+				else
+					System.out.println("The rectangle is not a square.");
+				
+				if(rect.contains(point))
+					System.out.println("The point is within the rectangle.");
+				else
+					System.out.println("The point is not within the rectangle.");
+			}
+			else{
+				System.out.println("Error: unrecognized shape type.");
+			}
+			
+			System.out.print("Would you like to create a new shape? (y/n): ");
+			moreInput = s.nextLine();
+		}
 	}
 	
 }
@@ -79,7 +141,7 @@ class Rectangle{
 	}
 	
 	boolean contains(Point p){
-		return (p.x > upperLeft.x && p.x < upperLeft.x+width && p.y > upperLeft.y && p.y < upperLeft.y+height);
+		return (p.x > upperLeft.x && p.x < upperLeft.x+width && p.y < upperLeft.y && p.y > upperLeft.y-height);
 	}
 }
 
